89b9089abe175c5616c2496d3df1f4b3d41aeac0
Gabriele Gargani <ggargani@c1r5p17.hive.fi>
2025-02-13 11:36:02 +0200

    So_Long

--- bin/map_validation.d
+++
@@ -1,4 +0,0 @@
-bin/map_validation.o: src/map_validation.c src/so_long.h \
-  src/../libraries/MLX42/include/MLX42/MLX42.h \
-  src/../libraries/libft/libft.h libraries/libft/ft_printf.h \
-  libraries/libft/get_next_line.h
--- bin/rendering.d
+++
@@ -1,4 +0,0 @@
-bin/rendering.o: src/rendering.c src/so_long.h \
-  src/../libraries/MLX42/include/MLX42/MLX42.h \
-  src/../libraries/libft/libft.h libraries/libft/ft_printf.h \
-  libraries/libft/get_next_line.h
